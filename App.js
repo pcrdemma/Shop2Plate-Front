0cd0c360b2e39a2c8d879c46ec640fe6e28bcc62
Emma Picard <emma.picard@ynov.com>
2024-03-21 09:23:40 +0100

    link between welcome page and login

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Login from './pages/Login.js';
-
 import Register from './pages/Register.js';
+import Welcome from './pages/Welcome.js';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { createStackNavigator } from '@react-navigation/stack';
-import Welcome from './pages/Welcome.js';
 
 const Stack = createStackNavigator();
 
@@ -13,12 +12,12 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
-       // <Login />
-      //<Welcome />
+
   );
 };
 
