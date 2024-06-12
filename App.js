1ff9626bb680d169218bf6f4490e4cbf40ae1b62
Emma PICARD <emma.picard@ynov.com>
2024-06-12 14:30:22 +0200

    page login connexion avec les datas, ok

--- App.js
+++ App.js
@@ -6,13 +6,11 @@ import Budget from './pages/Budget.js';
 import Register from './pages/Register.js';
 import Login from './pages/Login.js';
 //import Account from './pages/Account.js';
+import AddStock from './pages/AddStock.js';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-import Stock from './pages/Stock.js';
-import Budget from './pages/Budget.js';
-import AddStock from './pages/AddStock.js';
 
 const Stack = createStackNavigator();
 
@@ -27,10 +25,10 @@ const App = () => {
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} /> 
         <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}/>
 
-//       <Stack.Navigator mode="modal">
-//         <Stack.Screen name="Stock" component={Stock} options={{ headerShown: false }} />
-//         <Stack.Screen name="Budget" component={Budget} options={{ headerShown: false }} />
-//         <Stack.Screen name="AddStock" component={AddStock} options={{ headerShown: false, presentation: 'modal' }} />
+{/*      <Stack.Navigator mode="modal">
+         <Stack.Screen name="Stock" component={Stock} options={{ headerShown: false }} />
+         <Stack.Screen name="Budget" component={Budget} options={{ headerShown: false }} />
+         <Stack.Screen name="AddStock" component={AddStock} options={{ headerShown: false, presentation: 'modal' }} /> */}
 
       </Stack.Navigator>
     </NavigationContainer>
