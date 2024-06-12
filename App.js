7808d1f5ec9c162cde71aafd463f688bf09b7dc3
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-06-12 16:39:06 +0200

    ajout bouton suppr + calcul budget

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import Stock from './pages/Stock.js'
 import Budget from './pages/Budget.js';
 import Register from './pages/Register.js';
 import Login from './pages/Login.js';
-//import Account from './pages/Account.js';
+import Account from './pages/Account.js';
 import AddStock from './pages/AddStock.js';
 
 
@@ -26,8 +26,8 @@ const App = () => {
         {/* <Stack.Screen name="Budget" component={Budget} options={{ headerShown: false }} /> */}
         {/* <Stack.Screen name="Account" component={Account} options={{ headerShown: false }} />*/}
         {/* <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} /> */}
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} /> 
-        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}/>
+        <Stack.Screen name="Account" component={Account} options={{ headerShown: false }} /> 
+        {/* <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}/> */}
 
 {/*      <Stack.Navigator mode="modal">
          <Stack.Screen name="Stock" component={Stock} options={{ headerShown: false }} />
