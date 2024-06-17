3fba62f9dd8ab4e611dbb798dcb18a0abd2feff3
Manon LE BARAZER <manonlebarazer@icloud.com>
2024-06-17 11:46:56 +0200

    ajout de la navbar +pop up

--- App.js
+++ App.js
@@ -1,24 +1,61 @@
-// App.js
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import { View, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import Stock from './pages/Stock';
+import Budget from './pages/Budget';
+import Account from './pages/Account';
+import ShopingList from './pages/ShopingList';
+import AddDepense from './pages/AddDepense';
 
-import Stock from './pages/Stock.js';
-import Budget from './pages/Budget.js';
-import AddStock from './pages/AddStock.js';
+const Tab = createBottomTabNavigator();
 
-const Stack = createStackNavigator();
-
-const App = () => {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator mode="modal">
-        <Stack.Screen name="Stock" component={Stock} options={{ headerShown: false }} />
-        <Stack.Screen name="Budget" component={Budget} options={{ headerShown: false }} />
-        <Stack.Screen name="AddStock" component={AddStock} options={{ headerShown: false, presentation: 'modal' }} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        initialRouteName="Budget"
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconSource;
+
+            if (route.name === 'Budget') {
+              iconSource = focused
+                ? require('./assets/moneyNav.png')
+                : require('./assets/moneyNav.png');
+            } else if (route.name === 'Stock') {
+              iconSource = focused
+                ? require('./assets/frigoNav.png')
+                : require('./assets/frigoNav.png');
+            } else if (route.name === 'ShopingList') {
+              iconSource = focused
+              ? require('./assets/panierNav.png')
+              : require('./assets/panierNav.png');
+            } else if (route.name === 'AddDepense') {
+              iconSource = focused
+                ? require('./assets/userNav.png')
+                : require('./assets/userNav.png');
+            }
+
+            return <Image source={iconSource} style={{ width: 24, height: 24 }} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            backgroundColor: '#fff',
+            borderTopColor: 'transparent',
+            elevation: 0, // for Android
+            shadowOpacity: 0.3, // for iOS
+          },
+        })}
+      >
+        <Tab.Screen name="Budget" component={Budget} />
+        <Tab.Screen name="Stock" component={Stock} />
+        <Tab.Screen name="ShopingList" component={ShopingList} />
+        <Tab.Screen name="AddDepense" component={AddDepense} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
-};
-
-export default App;
+}
---
+++ pages/Nav.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { style } from '../components/navStyle.js';
+import { View, Text, Dimensions, Image, TextInput, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+
+
+const Nav = () => {
+    return (
+        <View>
+
+        </View>
+
+    );
+};
+
+export default Nav;
